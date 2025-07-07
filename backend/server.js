5d9984a957030a94244aa24d75a47aa980f8a61f
Junior Wilson <seuemail@example.com>
2025-07-07 17:01:21 -0300

    dontenv att backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const { sequelize } = require("./models/usuario");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+console.log(process.env.PORT);
 
 app.use(cors());
 app.use(express.json());
