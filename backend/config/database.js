6b126e2da5f29d3c20c8be6043fa627bc7f7f3e6
Junior Wilson <seuemail@example.com>
2025-07-07 17:06:57 -0300

    alteração

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("perfilusuario", "root", "1234", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 sequelize
   .authenticate()
@@ -11,4 +16,3 @@ sequelize
   .catch((err) => console.error("Erro na conexão com Sequelize:", err));
 
 module.exports = sequelize;
- 
